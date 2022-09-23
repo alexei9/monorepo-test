e57b55a1650c1a6123ed42caef58008ca6ce53ec
Alex George <alex.george@cancer.org.uk>
2022-09-23 16:22:45 +0100

    fixing flake8 bug to test on push

--- project1/project1_app/main.py
+++ project1/project1_app/main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# ions, and settings and a test for alex that the length is way. ions, and settings and a test for alex that the length is way. ions, and settings and a test for alex that the length is way. ions, and settings and a test for alex that the length is way.
+# ions, and settings and a test for alex that the length is way. ions, and
 
 
 def print_hi(name):
