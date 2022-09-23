7a94bf533175b87bd1b6e188f8adb6564c67b265
Alex George <alex.george@cancer.org.uk>
2022-09-23 17:01:30 +0100

    testing GitHub actions

--- project2/main.py
+++ project2/main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings this error is from Project2 soft fudge1
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and ettings this error is from Project2 soft fudge1
 
 
 def print_hi(name):
