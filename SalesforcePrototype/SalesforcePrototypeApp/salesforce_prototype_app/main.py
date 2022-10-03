12dcf7f8a9443cfa5b80b5def72732cd8e37fa66
Alex George <alex.george@cancer.org.uk>
2022-10-03 17:48:32 +0100

    2nd Updation to test ELT

--- SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/main.py
+++ SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Alex George - Updation')
+    print_hi('Alex George - Updation - 6pm attempt')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
