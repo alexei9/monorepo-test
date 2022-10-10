6e7f98e6f3d78c3f1a2d420c39de0fa8a7a11e05
Alex George <alex.george@cancer.org.uk>
2022-10-10 12:38:10 +0100

    Added in is_running_in_container print statement to test

--- SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/main.py
+++ SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/main.py
@@ -1,4 +1,5 @@
 from salesforce_prototype_app.utilities.get_connections import get_boto3
+from salesforce_prototype_app.utilities.app_environment import is_running_in_container
 
 
 def print_hi(name):
@@ -9,6 +10,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Alex George - Updation - toucan attempt')
+    print(is_running_in_container())
     print(get_boto3())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
