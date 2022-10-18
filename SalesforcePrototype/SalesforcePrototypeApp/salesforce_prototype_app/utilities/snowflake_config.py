c627c97c8659481674f801b62bc67105e2a4c5bf
Alex George <alex.george@cancer.org.uk>
2022-10-18 15:58:09 +0100

    Minor tweak to Config table creation script

---
+++ SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/utilities/snowflake_config.py
@@ -0,0 +1,15 @@
+from salesforce_prototype_app.utilities.rsa_tools import get_user_secret_from_aws, get_snowflake_rsa_keys_connection
+
+
+
+def get_valid_entities():
+    secret_dict = get_user_secret_from_aws()
+    # connect to snowflake as service user and read Snowflake version
+    con = get_snowflake_rsa_keys_connection(secret_dict)
+    cursor = con.cursor()
+    get_column_names_sql = f'SELECT TOP 1 * FROM DEV_AG_SALESFORCE.SALESFORCE_LOAD.{destination_table_name}'
+    execute_get_column_names = cursor.execute(get_column_names_sql)
+    column_names = [column[0] for column in execute_get_column_names.description]
+    con.close()
+    print(column_names)
+    return column_names
