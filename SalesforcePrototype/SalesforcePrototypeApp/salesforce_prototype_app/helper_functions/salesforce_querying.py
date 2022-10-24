258b048c71f5454947c6d2d69190f786fe35beff
Alex George <alex.george@cancer.org.uk>
2022-10-24 16:07:57 +0100

    Added in some helper functions for querying Salesforce objects and fields

---
+++ SalesforcePrototype/SalesforcePrototypeApp/salesforce_prototype_app/helper_functions/salesforce_querying.py
@@ -0,0 +1,33 @@
+from salesforce_prototype_app.utilities.get_connections import get_salesforce, get_boto3
+from simple_salesforce import SFType
+from tabulate import tabulate
+import salesforce_prototype_app.utilities.app_environment as app_env
+
+
+
+def salesforce_query():
+
+    sf = get_salesforce()
+    valid_contact_fields_sql = f"SELECT ID, FIRSTNAME, LASTNAME FROM Contact WHERE Id = '0032500001eyiEkAAI'"
+    results = sf.query_all(valid_contact_fields_sql)
+    rows = results['records']
+    for row in rows:
+        del row['attributes']
+    for row in rows:
+        print(row)
+
+    objects = sf.describe()
+
+    print(f'{len(objects["sobjects"])} objects found.')
+    for obj in objects["sobjects"]:
+        print(obj["label"])
+
+sf = get_salesforce()
+object_name = input('Specify object name: ')
+sf_type = SFType(object_name, session_id=sf.session_id, sf_instance=sf.sf_instance)
+description = sf_type.describe()
+fields = description['fields']
+table_data = list(map(lambda x: [x['name'], x['label'], x['type']], fields))
+print(tabulate(table_data, headers=['Name', 'Label', 'SF Data Type']))
+
+
